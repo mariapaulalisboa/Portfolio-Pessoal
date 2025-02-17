92bca6c3d80e69998a17abd281586162db466716
Maria Paula Lisboa <mariapaula1704@gmail.com>
2025-02-17 13:46:02 -0300

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 document.getElementById('nav-toggle').addEventListener('click', function() {
     document.getElementById('nav-menu').classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const projectItems = document.querySelectorAll('#projects .projects-item');
+
+    projectItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const overlay = this.querySelector('.overlay');
+            overlay.classList.toggle('active');
+        });
+    });
+});
